92f3c0f27a4568bd75b6b0422874d767febe7306
LucasMorlet <62259047+LucasMorlet@users.noreply.github.com>
2025-03-19 16:21:20 +0100

    Reformating VideoStream code

--- HIVE/Assets/AR/VideoStream.cs
+++ HIVE/Assets/AR/VideoStream.cs
@@ -1,84 +1,98 @@
 using UnityEngine;
 using System;
 using System.Collections;
-using UnityEngine;
 #if UNITY_ANDROID
 using UnityEngine.Android;
 #endif
 
-
-
 public class VideoStream : MonoBehaviour
 {
-    WebCamDevice[] camera_list;
-    WebCamTexture camera_texture;
-    UnityEngine.UI.RawImage rendered_image;
+    // Attributes
+    private WebCamDevice[] camera_list;
+    private WebCamTexture camera_texture;
+    private UnityEngine.UI.RawImage rendered_image;
 
 
-#if UNITY_IOS || UNITY_WEBGL
-    private bool CheckPermissionAndRaiseCallbackIfGranted(UserAuthorization authenticationType, Action authenticationGrantedAction)
-    {
-        if (Application.HasUserAuthorization(authenticationType))
+    #if UNITY_IOS || UNITY_WEBGL
+        private bool CheckPermissionAndRaiseCallbackIfGranted(UserAuthorization authenticationType, Action authenticationGrantedAction)
         {
-            if (authenticationGrantedAction != null)
-                authenticationGrantedAction();
+            if (Application.HasUserAuthorization(authenticationType))
+            {
+                if (authenticationGrantedAction != null)
+                    authenticationGrantedAction();
 
-            return true;
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    private IEnumerator AskForPermissionIfRequired(UserAuthorization authenticationType, Action authenticationGrantedAction)
-    {
-        if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+        private IEnumerator AskForPermissionIfRequired(UserAuthorization authenticationType, Action authenticationGrantedAction)
         {
-            yield return Application.RequestUserAuthorization(authenticationType);
             if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
-                Debug.LogWarning($"Permission {authenticationType} Denied");
+            {
+                yield return Application.RequestUserAuthorization(authenticationType);
+                if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+                    Debug.LogWarning($"Permission {authenticationType} Denied");
+            }
         }
-    }
-#elif UNITY_ANDROID
-    private void PermissionCallbacksPermissionGranted(string permissionName)
-    {
-        StartCoroutine(DelayedCameraInitialization());
-    }
 
-    private IEnumerator DelayedCameraInitialization()
-    {
-        yield return null;
-        InitializeCamera();
-    }
-
-    private void PermissionCallbacksPermissionDenied(string permissionName)
-    {
-        Debug.LogWarning($"Permission {permissionName} Denied");
-    }
-
-    private void AskCameraPermission()
-    {
-        var callbacks = new PermissionCallbacks();
-        callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
-        callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
-        Permission.RequestUserPermission(Permission.Camera, callbacks);
-    }
-#endif
-
-    void Start()
-    {
-        #if UNITY_IOS || UNITY_WEBGL
-            StartCoroutine(AskForPermissionIfRequired(UserAuthorization.WebCam, () => { InitializeCamera(); }));
+        private void PermissionHandling ( )
+        {
+            StartCoroutine(AskForPermissionIfRequired(UserAuthorization.WebCam, () => { InitializeCamera(); })); // Doubt on still working after updating of core script
             return;
-        #elif UNITY_ANDROID
+        }
+
+
+    #elif UNITY_ANDROID
+        private void PermissionCallbacksPermissionGranted(string permissionName)
+        {
+            StartCoroutine(DelayedCameraInitialization());
+        }
+
+        private IEnumerator DelayedCameraInitialization()
+        {
+            yield return null;
+            InitializeCamera();
+        }
+
+        private void PermissionCallbacksPermissionDenied(string permissionName)
+        {
+            Debug.LogWarning($"Permission {permissionName} Denied");
+        }
+
+        private void AskCameraPermission()
+        {
+            var callbacks = new PermissionCallbacks();
+            callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
+            callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
+            Permission.RequestUserPermission(Permission.Camera, callbacks);
+        }
+
+        private void PermissionHandling ( )
+        {
             if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
             {
                 AskCameraPermission();
                 return;
             }
-        #endif
-        InitializeCamera();
+        }
+    
+    #else // GameMode context
+
+        private void PermissionHandling ( )
+        {
+            return;
+        }
+
+    #endif
+
+    void Start()
+    {
+        PermissionHandling(); // Depends on OS context
+        DisplayCameraList();
     }
 
-    private void InitializeCamera()
+    private void DisplayCameraList ()
     {
         // Display camera list
         camera_list = WebCamTexture.devices;
@@ -87,68 +101,53 @@ public class VideoStream : MonoBehaviour
             Debug.Log ( "No camera found");
             return;
         }
-        for (int i = 0; i < camera_list.Length; i++)
+        else
         {
-            Debug.Log(camera_list[i].name);
+            String str = "\n";
+            for (int i = 0; i < this.camera_list.Length; i++)
+            {
+                str += "Camera number ";
+                str += i;
+                str += " : ";
+                str += this.camera_list[i].name;
+                str += "\n";
+            }
+            Debug.Log(str);
         }
-
-
-        /*
-        camera_texture = new WebCamTexture(camera_list[1].name);
-        rendered_image = GetComponent<UnityEngine.UI.RawImage>();
-        rendered_image.texture = camera_texture;
-        camera_texture.Play();*/
     }
-
-
 
     void Update()
     {
-        /*
-        Event e = Event.current;
-        if ( e != null )
-        {
-            Debug.Log(e);
-        } 
-        */
         OnKeyDown();
     }
 
+    // Keyboard control
     void OnKeyDown ()
-    {
-        /*
-        KeyCode key = event.keyCode;
-        Debug.Log ( key );
-        if ( KeyCode.Keypad0 <= key && key <= KeyCode.Keypad9 )
-        {
-            int camera_number = (int)( key - KeyCode.Keypad0 );
-            if ( camera_number < camera_list.Length ) 
-            {
-                Debug.Log ( "New camera input :" + camera_list[camera_number] );
-                camera_texture = new WebCamTexture(camera_list[camera_number].name);
-                rendered_image = GetComponent<UnityEngine.UI.RawImage>();
-                rendered_image.texture = camera_texture;
-                camera_texture.Play();
-            }
-        }
-        */
-        
+    {       
         // Change camera with numpad
-        for ( int i = 0 ; i < camera_list.Length ; i++ ) 
+        for ( int i = 0 ; i <=9 ; i++ ) 
         {
             if ( Input.GetKeyDown ( KeyCode.Keypad0 + i ) )
             {
-                Debug.Log ( "New camera input :" + camera_list[i].name );
-                camera_texture = new WebCamTexture(camera_list[i].name);
-                rendered_image = GetComponent<UnityEngine.UI.RawImage>();
-                rendered_image.texture = camera_texture;
-                camera_texture.Play();
+                this.InitializeCamera ( i );
             }
         }
+    }
 
-        if ( Input.GetKeyDown ( KeyCode.Space ) )
+    private void InitializeCamera ( int cam_number )
+    {
+        if ( cam_number < this.camera_list.Length )
         {
-            Debug.Log("Coucou");
+            Debug.Log ( "New camera input : " + this.camera_list[cam_number].name );
+            this.camera_texture = new WebCamTexture( this.camera_list[cam_number].name );
+            this.rendered_image = this.GetComponent<UnityEngine.UI.RawImage>();
+            this.rendered_image.texture = this.camera_texture;
+            Debug.Log ( this.rendered_image.texture );
+            this.camera_texture.Play();
+        }
+        else 
+        {
+            Debug.LogWarning ( "Camera number " + cam_number + "does not exist, only " + this.camera_list.Length + " camera(s) found" );
         }
     }
 }
